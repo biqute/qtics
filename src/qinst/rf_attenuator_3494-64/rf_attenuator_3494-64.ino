8c704b6b7db901666f4d605e6ac791ee63a05e95
pietro <campana.pietro@gmail.com>
2023-11-14 15:05:43 +0100

    switch firmware SCPI

--- src/qinst/rf_attenuator_3494-64/rf_attenuator_3494-64.ino
+++ src/qinst/rf_attenuator_3494-64/rf_attenuator_3494-64.ino
@@ -1,10 +1,12 @@
-
-/* CONNECTIONS 11 PINS
- *
+/* 
+RF attenuator 3494-6
+BASIC USAGE: send the required attenuation in dB 
+CONNECTIONS: 11 PINS
+AVAILABLE RANGE: 0 to -64 dB
  */
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pins as outputs.
   for (int i=13; i >= 3; i--){
     pinMode(i, OUTPUT);
   }
@@ -12,6 +14,7 @@ void setup() {
 }
 
 float read_attenuation(float val){
+  // enforce range limits
   if (val > 64) val = 64;
   if (val < 0) val = 0;
   return val;
@@ -20,7 +23,7 @@ float read_attenuation(float val){
 const float map_db[11] = {0.03, 0.06, 0.13, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0};
 
 int * find_bitstring(float val){
-
+  // convert float decimal to binary considering the pin mapping
   static int bitstring[11];
 
   for (int i = 10; i >= 0; i-- ){
@@ -40,13 +43,11 @@ int * find_bitstring(float val){
 
 
 void loop() {
-
+  // wait for input from serial port, then set attenuation
   if (Serial.available()) {
 
     float attenuation = read_attenuation(Serial.parseFloat());
-
     int *binaryNum = find_bitstring (attenuation);
-
     for (int i = 0; i < 11; i++){
       if (binaryNum[i] == 1){
         digitalWrite(i+3, HIGH);
@@ -59,7 +60,6 @@ void loop() {
 
     }
   Serial.println("\n\n");
-
   delay(10);
   }
 
