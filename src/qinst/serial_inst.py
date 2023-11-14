c091efd0d4ca083e16ba72abf0e8a24875d9e3c3
marcoogobbo <gobbo.marco98@gmail.com>
2023-11-14 10:27:23 +0100

    Updated __init__ and minor changes in serial_inst.py

--- src/qinst/serial_inst.py
+++ src/qinst/serial_inst.py
@@ -16,46 +16,34 @@ class SerialInst(Instrument):
     ):
         super().__init__(name, address)
 
-        self.baudrate = baudrate
-        self.bytesize = bytesize
-        self.parity = parity
-        self.stopbits = stopbits
-        self.timeout = timeout
+        self.serial = serial.Serial()
+        self.serial.port = address
+        self.serial.baudrate = baudrate
+        self.serial.bytesize = bytesize
+        self.serial.parity = parity
+        self.serial.stopbits = stopbits
+        self.serial.timeout = timeout
+
         self.sleep = sleep
 
     def __del__(self):
         self.disconnected()
 
     def connect(self):
-        self.serial = serial.Serial()
-        self.serial.port = self.address
-        self.serial.badurate = self.baudrate
-        self.serial.parity = self.parity
-        self.serial.stopbits = self.stopbits
-        self.serial.timeout = self.timeout
-
         self.serial.open()
-
-        return
 
     def disconnect(self):
         if self.serial.isOpen():
             self.serial.close()
 
-        del self.serial
-
-        return
-
     def write(self, cmd):
         if self.serial is not None:
             self.serial.write((cmd + "\n").encode())
 
-        return
-
     def read(self):
         return (
             self.serial.read(self.serial.inWaiting())
-            if self.__ser is not None
+            if self.serial is not None
             else None
         )
 
