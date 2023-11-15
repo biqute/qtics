b03b813ede769a4c69d79d7cd189f92ed2363a29
Rodolfo Carobene <rodolfo.carobene@gmail.com>
2023-11-15 10:48:26 +0100

    simplify instruments imports

---
+++ src/qinst/__init__.py
@@ -0,0 +1,7 @@
+"""List of supported instruments."""
+
+from qinst.rf_attenuator_3494_64.rf_attenuator_3494_64 import attenuator_3494_64
+from qinst.rf_switch_R591722600.rf_switch_R591722600 import switch_R591
+
+Radial_R591722600 = switch_R591
+Kratos_349464 = attenuator_3494_64
