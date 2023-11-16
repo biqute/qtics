d91a674e1c552894b7eeb447a50817135144d993
Rodolfo Carobene <rodolfo.carobene@gmail.com>
2023-11-16 15:16:37 +0100

    docstrings

--- src/qinst/instrument.py
+++ src/qinst/instrument.py
@@ -1,21 +1,32 @@
+"""Base Instrument."""
+
+
 class Instrument:
+    """Base instrument class."""
+
     def __init__(self, name: str, address: str):
+        """Initialize."""
         self.name = name
         self.address = address
 
     def connect(self):
+        """Connect to the instrument."""
         raise NotImplementedError
 
     def disconnect(self):
+        """Disonnect from the instrument."""
         raise NotImplementedError
 
     def write(self):
+        """Send a command to the instrument."""
         raise NotImplementedError
 
     def read(self):
+        """Read from the instrument."""
         raise NotImplementedError
 
-    def query(self):
+    def query(self, _):
+        """Send a command and read from the instrument."""
         raise NotImplementedError
 
     def get_id(self):
