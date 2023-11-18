772ec0f8d423dfb7373c8f28d5fb064b5814d80f
pietro <campana.pietro@gmail.com>
2023-11-18 17:10:30 +0100

    Basic VNA driver

--- src/qinst/__init__.py
+++ src/qinst/__init__.py
@@ -1,5 +1,6 @@
 """List of supported instruments."""
 
+from qinst.network.NA_N9916A import VNA9916A
 from qinst.serial.rf_attenuator_3494_64.rf_attenuator_3494_64 import Attenuator_3494_64
 from qinst.serial.rf_switch_R591722600.rf_switch_R591722600 import Switch_R591
 from qinst.serial.voltage_source_SIM928 import SIM928
@@ -7,3 +8,4 @@ from qinst.serial.voltage_source_SIM928 import SIM928
 Radiall_R591722600 = Switch_R591
 Kratos_349464 = Attenuator_3494_64
 SRS_SIM928 = SIM928
+VNA_9916A = VNA9916A
