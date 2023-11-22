a194607baf38dc4fc2a01aa07dd2c6c03b982d4b
marcoogobbo <gobbo.marco98@gmail.com>
2023-11-22 18:06:24 +0100

    update RS_SMA100B.py

--- src/qinst/__init__.py
+++ src/qinst/__init__.py
@@ -1,6 +1,7 @@
 """List of supported instruments."""
 
 from qinst.network.NA_N9916A import VNA9916A
+from qinst.network.RS_SMA100B import SMA100B
 from qinst.serial.rf_attenuator_3494_64.rf_attenuator_3494_64 import Attenuator_3494_64
 from qinst.serial.rf_switch_R591722600.rf_switch_R591722600 import Switch_R591
 from qinst.serial.voltage_source_SIM928 import SIM928
@@ -9,3 +10,4 @@ Radiall_R591722600 = Switch_R591
 Kratos_349464 = Attenuator_3494_64
 SRS_SIM928 = SIM928
 VNA_9916A = VNA9916A
+SMA_100B = SMA100B
