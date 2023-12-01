17813c0634522569da979af38656070c2a704d81
pietro <campana.pietro@gmail.com>
2023-12-01 17:05:33 +0100

    minor error fix

--- src/qinst/__init__.py
+++ src/qinst/__init__.py
@@ -7,7 +7,7 @@ from qinst.network.triton_ctrl import Triton
 from qinst.serial.keithley6514 import Keithley6514
 from qinst.serial.rf_attenuator_3494_64.rf_attenuator_3494_64 import Attenuator_3494_64
 from qinst.serial.rf_switch_R591722600.rf_switch_R591722600 import Switch_R591
-from qinst.serial.synth_FSW0010 import FSW0010
+from qinst.serial.synth_FSL0010 import FSL0010
 from qinst.serial.voltage_source_SIM928 import SIM928
 
 Radiall_R591722600 = Switch_R591
