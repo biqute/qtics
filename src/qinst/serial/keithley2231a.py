44d039087584ec6578a1ddf8b0bb467019591e38
marcoogobbo <gobbo.marco98@gmail.com>
2023-12-02 10:47:10 +0100

    applying suggestions from review

--- src/qinst/serial/keithley2231a.py
+++ src/qinst/serial/keithley2231a.py
@@ -5,6 +5,7 @@ Controller of the Keithley2231A DC Power Supply.
 .. moduleauthor:: Marco Gobbo <marco.gobbo@mib.infn.it>
 """
 
+from qinst import log
 from qinst.serial_inst import SerialInst
 
 
@@ -15,12 +16,14 @@ class Keithley2231A(SerialInst):
         """Put Keithley 2231A DC Power Supply in remote."""
         self.serial.open()
         self.write("SYST:REM")
+        log.info(f"Instrument {self.name} connected succesfully.")
 
     def disconnect(self):
         """Take Keithley 2231A DC Power Supply out of remote."""
         if self.serial.is_open:
             self.write("SYST:LOC")
             self.serial.close()
+            log.info(f"Instrument {self.name} disconnected.")
 
     @property
     def is_completed(self) -> bool:
@@ -44,54 +47,48 @@ class Keithley2231A(SerialInst):
         self.write("*WAI")
 
     @property
-    def ch_select(self) -> str:
+    def channel(self) -> int:
         """Select the channel to use."""
-        return self.query("INST:NSEL?")
+        return int(self.query("INST:NSEL?").split()[-1])
 
-    @ch_select.setter
-    def ch_select(self, ch: int):
+    @channel.setter
+    def channel(self, ch: int):
         self.write(f"INST:NSEL {self.validate_opt(ch, (1,2,3))}")
 
     @property
     def voltage(self) -> float:
         """Voltage of the selected the channel."""
-        return self.query("SOUR:VOLT:LEV:IMM:AMPL?")
+        return float(self.query("SOUR:VOLT:LEV:IMM:AMPL?"))
 
     @voltage.setter
-    def voltage(self, ch: int, value: float):
-        self.ch_select = self.validate_opt(ch, (1, 2, 3))
-        self.write(
-            f"SOUR:VOLT:LEV:IMM:AMPL {self.validate_range(value, 0, 5) if ch == 3 else self.validate_range(value, 0, 30)}"
-        )
+    def voltage(self, value: float):
+        v_max = 5 if self.channel == 3 else 30
+        self.write(f"SOUR:VOLT:LEV:IMM:AMPL {self.validate_range(value, 0, v_max)}")
 
     @property
     def current(self) -> float:
         """Current of the selected the channel."""
-        return self.query("SOUR:CURR:LEV:IMM:AMPL?")
+        return float(self.query("SOUR:CURR:LEV:IMM:AMPL?"))
 
     @current.setter
-    def current(self, ch: int, value: float):
-        self.ch_select = self.validate_opt(ch, (1, 2, 3))
+    def current(self, value: float):
         self.write(f"SOUR:CURR:LEV:IMM:AMPL {self.validate_range(value, 0, 3)}")
 
     @property
     def voltage_limit(self) -> float:
         """Voltage limit of the selected the channel."""
-        return self.query("SOUR:VOLT:LIMIT:LEV?")
+        return float(self.query("SOUR:VOLT:LIMIT:LEV?"))
 
     @voltage_limit.setter
-    def voltage_limit(self, ch: int, value: float):
-        self.ch_select = self.validate_opt(ch, (1, 2, 3))
-        self.write(
-            f"SOUR:VOLT:LIMIT:LEV {self.validate_range(value, 0, 5) if ch == 3 else self.validate_range(value, 0, 30)}"
-        )
+    def voltage_limit(self, value: float):
+        v_max = 5 if self.channel == 3 else 30
+        self.write(f"SOUR:VOLT:LIMIT:LEV {self.validate_range(value, 0, v_max)}")
 
     @property
     def current_limit(self) -> float:
         """Current limit of the selected the channel."""
-        return self.query("SOUR:CURR:LIMIT:LEV?")
+        return float(self.query("SOUR:CURR:LIMIT:LEV?"))
 
     @current_limit.setter
-    def current_limit(self, ch: int, value: float):
-        self.ch_select = self.validate_opt(ch, (1, 2, 3))
+    def current_limit(self, value: float):
         self.write(f"SOUR:VOLT:LIMIT:LEV {self.validate_range(value, 0, 3)}")
