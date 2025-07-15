ddaf08b8f6ffd7b5e05fed4af80750b0ecc8f10d
Rodolfo Carobene <rodolfo.carobene@gmail.com>
2025-07-15 16:47:17 +0200

    changes done with hardware tests

--- src/qtics/instruments/serial/valon_5019.py
+++ src/qtics/instruments/serial/valon_5019.py
@@ -11,6 +11,17 @@ import serial
 from qtics.instruments import SerialInst
 
 
+def freq_result_to_hz(res):
+    """Convert list [value, unit] to Hz value."""
+    if res[1] == "MHz":
+        return float(res[0]) * 1e6
+    if res[1] == "KHz":
+        return float(res[0]) * 1e3
+    if res[1] == "Hz":
+        return float(res[0])
+    return res
+
+
 class VALON5019(SerialInst):
     """Frequency synthesizer driver for VALON5019."""
 
@@ -23,29 +34,37 @@ class VALON5019(SerialInst):
         parity: Literal["N"] = serial.PARITY_NONE,
         stopbits: int = serial.STOPBITS_ONE,
         timeout: int = 10,
-        sleep: float = 0.1,
+        sleep: float = 0.3,
     ):
         """Initialize."""
         super().__init__(
             name, address, baudrate, bytesize, parity, stopbits, timeout, sleep, "\r"
         )
+        self.last_cmd_lenght = 0
+
+    def get_id(self):
+        """Return name of the device from SCPI standard query."""
+        return self.query("ID")[0]
 
     def write(self, cmd, sleep=False):
         """Write a message to the serial port."""
-        self.last_cmd_lenght = len(cmd + self.terminator)
+        self.last_cmd_lenght = len(cmd + self.terminator) + 2
         super().write(cmd, sleep)
 
-    def read(self) -> str:
+    def read(self):
         """Read a message from the serial port."""
         res = super().read()
         if res != "":
-            res = res[self.last_cmd_lenght :].split(";")[0]
-        return res
+            res = res[self.last_cmd_lenght :]
+            res = res.split(";")[0]
+            res = res.split("\r\n\r-->")[0]
+            res = res.split("")
+        return res[1:]
 
     @property
     def mode(self):
         """Return current mode of operation (continuous tone, sweep, list)."""
-        return self.query("MOD?")
+        return self.query("MOD")[0]
 
     @mode.setter
     def mode(self, mode_to_set):
@@ -57,7 +76,8 @@ class VALON5019(SerialInst):
     @property
     def freq(self):
         """Output signal frequency in Hz."""
-        return float(self.query("F?")) * 1e6
+        res = self.query("F")
+        return freq_result_to_hz(res)
 
     @freq.setter
     def freq(self, freq: float):
@@ -68,67 +88,72 @@ class VALON5019(SerialInst):
     @property
     def freq_offset(self):
         """Offset frequency that can be added or subtracted to the frequency."""
-        return float(self.query("OFF?")) * 1e6
+        res = self.query("OFF")
+        return freq_result_to_hz(res)
 
     @freq_offset.setter
     def freq_offset(self, freq_to_set):
+        freq_to_set = self.validate_range(freq_to_set, 0, 4.25e9)
         self.write(f"OFF {freq_to_set}Hz")
 
     @property
     def freq_step(self):
         """Step frequency for the CW mode. Used with increment/decrement."""
-        return float(self.query("FS?")) * 1e6
+        res = self.query("FS")
+        return freq_result_to_hz(res)
 
     @freq_step.setter
     def freq_step(self, freq_to_set):
+        freq_to_set = self.validate_range(freq_to_set, 0, 4e9)
         self.write(f"FS {freq_to_set}Hz")
 
-    def set_freq_increment(self):
+    def increment_freq(self):
         """In CW mode, step frequency is used for increment."""
-        self.write("FINC")
+        res = self.query("FINC")
+        return freq_result_to_hz(res)
 
-    def set_freq_decrement(self):
+    def decrement_freq(self):
         """In CW mode, step frequency is used for decrement."""
-        self.write("FDEC")
-
-    def get_cw_direction(self):
-        """Check direction of CW sweep."""
-        ret = self.query("FINC?")
-        if ret == "1":
-            return "Increment"
-        return "Decrement"
+        res = self.query("FDEC")
+        return freq_result_to_hz(res)
 
     @property
     def sweep_start(self):
         """Start frequency for Sweep mode."""
-        return float(self.query("STAR?")) * 1e6
+        res = self.query("STAR")
+        return freq_result_to_hz(res)
 
     @sweep_start.setter
     def sweep_start(self, freq_to_set):
+        freq_to_set = self.validate_range(freq_to_set, 0, 42.5e9)
         self.write(f"STAR {freq_to_set}Hz")
 
     @property
     def sweep_stop(self):
         """Stop frequency for Sweep mode."""
-        return float(self.query("STOP?")) * 1e6
+        res = self.query("STOP")
+        return freq_result_to_hz(res)
 
     @sweep_stop.setter
     def sweep_stop(self, freq_to_set):
+        freq_to_set = self.validate_range(freq_to_set, 0, 42.5e9)
         self.write(f"STOP {freq_to_set}Hz")
 
     @property
     def sweep_step(self):
         """Step frequency for Sweep mode."""
-        return float(self.query("STEP?")) * 1e6
+        res = self.query("STEP")
+        return freq_result_to_hz(res)
 
     @sweep_step.setter
     def sweep_step(self, freq_to_set):
+        freq_to_set = self.validate_range(freq_to_set, 10, 4.25e9)
         self.write(f"STEP {freq_to_set}Hz")
 
     @property
     def sweep_rate(self):
         """Sweep rate in milliseconds for Sweep mode."""
-        return float(self.query("RATE?"))
+        return float(self.query("RATE")[0])
 
     @sweep_rate.setter
     def sweep_rate(self, rate_to_set):
@@ -137,7 +162,7 @@ class VALON5019(SerialInst):
     @property
     def sweep_rtime(self):
         """Sweep retrace time."""
-        return float(self.query("RTIME?"))
+        return float(self.query("RTIME")[0])
 
     @sweep_rtime.setter
     def sweep_rtime(self, time_to_set):
@@ -160,11 +185,11 @@ class VALON5019(SerialInst):
         External trigger  expects a positive edge on USER PORT.
         External steps waits for trigger and increments frequency by step
         """
-        return self.query("TMOD?")
+        return self.query("TMOD")[0]
 
     @sweep_tmode.setter
     def sweep_tmode(self, value):
-        if value not in ("AUT0", "MAN", "EXT", "EXTS"):
+        if value not in ("AUTO", "MAN", "EXT", "EXTS"):
             raise ValueError(f"Mode {value} non existent")
         self.write(f"TMOD {value}")
 
@@ -175,68 +200,80 @@ class VALON5019(SerialInst):
     @property
     def list_entry(self):
         """Frequencies in the table for list mode."""
-        ret = self.query("L?")
+        raise NotImplementedError("READING DOESN'T WORK")
+        ret = self.query("LI")
         return ret
 
     @list_entry.setter
     def list_entry(self, frequencies, powers=None):
         for idx, freq in frequencies:
             if powers is None:
-                self.write(f"L {idx} {freq}Hz")
+                self.write(f"LI {idx+1} {freq}Hz")
             else:
-                self.write(f"L {idx} {freq}Hz {powers[idx]}")
+                self.write(f"LI {idx+1} {freq}Hz {powers[idx]}")
         self.write("SAVE")
 
     @property
     def power(self):
         """Power of the output in dBm."""
-        return float(self.query("PWR?"))
+        return float(self.query("PWR")[0])
 
     @power.setter
     def power(self, pow_to_set):
+        pow_to_set = self.validate_range(pow_to_set, -50, 20)
         self.write(f"PWR {pow_to_set}")
 
     @property
     def power_oen(self):
         """Enable or disable the RF output buffer amplifiers while leaving the synthesizer PLL locked."""
-        return self.query("OEN")
+        return self.query("OEN")[0] == "1"
 
     @power_oen.setter
     def power_oen(self, value):
-        self.write(f"OEN {value}")
+        if value in (True, 1, "True"):
+            self.write(f"OEN 1")
+        elif value in (False, 0, "False"):
+            self.write(f"OEN 0")
+        raise ValueError(f"Value {value} not supported.")
 
     @property
     def power_pdn(self):
         """Place the synthesizer in a low power mode by disabling most of the synthesizer internal circuits."""
-        return self.query("PDN")
+        return self.query("PDN")[0] == "1"
 
     @power_pdn.setter
     def power_pdn(self, value):
-        self.write(f"PDN {value}")
+        if value in (True, 1, "True"):
+            self.write(f"PDN 1")
+        elif value in (False, 0, "False"):
+            self.write(f"PDN 0")
+        raise ValueError(f"Value {value} not supported.")
 
     @property
     def am_modulation(self):
         """Enables AM modulation (value in dB)."""
-        return float(self.query("AMD?"))
+        return float(self.query("AMD")[0])
 
     @am_modulation.setter
     def am_modulation(self, value):
+        value = self.validate_range(value, 0, 50)
         self.write(f"AMD {value}")
 
     @property
     def am_frequency(self):
         """AM frequency from 1Hz to 2 kHz."""
-        return float(self.query("AMF?"))
+        return float(self.query("AMF"))
 
     @am_frequency.setter
     def am_frequency(self, freq):
-        freq = self.validate_range(freq, 1, 2e3)
+        freq = self.validate_range(freq, 0.5, 1e4)
         self.write(f"AMF {freq}")
 
     @property
     def ref_freq(self):
         """Expected value of the external reference frequency."""
-        return self.query("REF?")
+        res = self.query("REF")
+        return freq_result_to_hz(res)
 
     @ref_freq.setter
     def ref_freq(self, value):
@@ -245,7 +282,7 @@ class VALON5019(SerialInst):
     @property
     def ref_source(self):
         """Check which clock reference is used."""
-        ret = int(self.query("REFS?"))
+        ret = int(self.query("REFS"))
         dict_par = {0: "Internal", 1: "External"}
         return dict_par[ret]
 
